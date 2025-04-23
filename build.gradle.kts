76605c74b540d9f57334d61195dfb25fdaabdac9
arngrame <arngrame.dev@gmail.com>
2025-04-23 23:26:16 +0400

    zip task added

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,19 @@ checkstyle {
     configFile = file("checkstyle.xml")
     isIgnoreFailures = false
 }
+
+application {
+    mainClass.set("ru/job4j/Main.java")
+}
+
+tasks.register<Zip>("zipJavaDoc") {
+    group = "documentation" // Группа, в которой будет отображаться задача
+    description = "Packs the generated Javadoc into a zip archive"
+
+    dependsOn("javadoc") // Указываем, что задача зависит от выполнения javadoc
+
+    from("build/docs/javadoc") // Исходная папка для упаковки
+    archiveFileName.set("javadoc.zip") // Имя создаваемого архива
+    destinationDirectory.set(layout.buildDirectory.dir("archives")) // Директория, куда будет сохранен архив
+}
+
--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+ * Class for running application
+ */
 public class Main {
+
+    /**
+     * Entry point of application
+     * @param args An array of input arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
